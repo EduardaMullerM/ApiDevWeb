a8a9915ae85a0706c1a6aefd6c45406071928894
Eduarda Muller <eduardamuller@icloud.com>
2023-09-11 10:28:24 -0300

    commit 7.1

--- mod_cliente/ClienteDAO.py
+++ mod_cliente/ClienteDAO.py
@@ -7,24 +7,76 @@ from mod_cliente.Cliente import Cliente
 
 router = APIRouter()
 
-# Criar as rotas/endpoints: GET, POST, PUT, DELETE
-
-@router.get("/cliente/", tags=["cliente"])
-def get_cliente():
-    return {"msg": "get todos executado"}, 200
-
-@router.get("/cliente/{id}", tags=["cliente"])
+@router.get("/cliente/{id}", tags=["Cliente"])
 def get_cliente(id: int):
-    return {"msg": "get um executado"}, 200
+    try:
+        session = db.Session()
+        # busca um com filtro
+        dados = session.query(ClienteDB).filter(ClienteDB.id_cliente == id).all()
+        return dados, 200
+    
+    except Exception as e:
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
 
-@router.post("/cliente/", tags=["cliente"])
-def post_cliente(c: Cliente):
-    return {"msg": "post executado", "nome": c.nome, "cpf": c.cpf, "telefone": c.telefone, "compra_fiado": c.compra_fiado, "dia_fiado": c.dia_fiado, "senha": c.senha}, 200
 
-@router.put("/cliente/{id}", tags=["cliente"])
-def put_cliente(id: int, c: Cliente):
-    return {"msg": "put executado", "id": id, "nome": c.nome, "cpf": c.cpf, "telefone": c.telefone, "compra_fiado": c.compra_fiado, "dia_fiado": c.dia_fiado, "senha": c.senha}, 201
+@router.post("/cliente/", tags=["Cliente"])
+def post_cliente(corpo: Cliente):
+    try:
+        session = db.Session()
+        dados = ClienteDB(None, corpo.nome, corpo.matricula, corpo.cpf, corpo.telefone, corpo.grupo, corpo.senha)
 
-@router.delete("/cliente/{id}", tags=["cliente"])
+        session.add(dados)
+
+        session.commit()
+
+        return {"id": dados.id_cliente}, 200
+    
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+
+@router.put("/cliente/{id}", tags=["Cliente"])
+def put_cliente(id: int, corpo: Cliente):
+    try:
+        session = db.Session()
+
+        dados = session.query(ClienteDB).filter(
+            ClienteDB.id_cliente == id).one()
+        
+        dados.nome = corpo.nome
+        dados.cpf = corpo.cpf
+        dados.compra_fiado = corpo.compra_fiado
+        dados.dia_fiado = corpo.dia_fiado
+        dados.senha = corpo.senha
+
+        session.add(dados)
+        session.commit()
+
+        return {"id": dados.id_cliente}, 200
+    
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+
+@router.delete("/cliente/{id}", tags=["Cliente"])
 def delete_cliente(id: int):
-    return {"msg": "delete executado"}, 201
+    try:
+        session = db.Session()
+
+        dados = session.query(ClienteDB).filter(ClienteDB.id_cliente == id).one()
+        session.delete(dados)
+        session.commit()
+
+        return {"id": dados.id_cliente}, 200
+    
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
